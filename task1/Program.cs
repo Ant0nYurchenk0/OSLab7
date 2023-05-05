1990f862cd5acf4132b23b6f2e5cbc7c3bca4727
Anton Yurchenko <anton.yurchenko@valtech.com>
2023-05-05 12:02:29 +0300

    done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,29 +1,32 @@
-﻿namespace OSLab7
+﻿using System.Threading;
+using System;
+
+namespace OSLab7
 {
   internal class Program
   {
     static void Main(string[] args)
     {
       int? resf = null;
-      var taskf = new Thread(() => { resf = f( 1); });
+      var taskf = new Thread(() => { resf = f(); });
       int? resg = null;
-      var taskg = new Thread(() => { resg = g( 1); });
+      var taskg = new Thread(() => { resg = g(1); });
       taskf.Start();
       taskg.Start();
       Console.WriteLine("Tasks started");
       var askToStop = true;
       var timeout = 5;
       int? res = null;
-      Thread.Sleep(timeout * 1000);
-      while ((!TaskEndedOrCanceled(taskf) || !TaskEndedOrCanceled(taskg)) && askToStop)
+      while (!TaskEndedOrCanceled(taskf) || !TaskEndedOrCanceled(taskg))
       {
         if ((resf.HasValue && resf.Value == 0) || (resg.HasValue && resg.Value == 0))
         {
           res = 0;
           break;
         }
+        Thread.Sleep(timeout * 1000);
         var responseIsValid = false;
-        while (!responseIsValid)
+        while (!responseIsValid && askToStop)
         {
           responseIsValid = true;
           Console.Write("Tasks are not ready yet. Write\n'a' to abort tasks,\n'b' to wait {0} seconds more,\n'c' to wait until completion\n>", timeout);
@@ -31,13 +34,11 @@
           switch (response)
           {
             case "a":
-              taskf.Interrupt();
-              taskg.Interrupt();
-              Thread.Sleep(1000);
+              taskf.Abort();
+              taskg.Abort();
               break;
             case "b":
-              Thread.Sleep(timeout * 1000);
-              break;
+              continue;
             case "c":
               askToStop = false;
               break;
@@ -51,12 +52,12 @@
         Console.WriteLine("Tasks were aborted");
       else
       {
-        taskf.Join();
-        taskg.Join();
         if (res == null)
-        {          
+        {
           res = resf * resg;
         }
+        taskf.Abort();
+        taskg.Abort();
         Console.WriteLine("Result: {0}", res);
       }
     }
@@ -77,18 +78,18 @@
     {
       switch (option)
       {
-        case 0: return 0;
+        case 0: return 1;
         case 1:
           for (var i = 0; i < 15; i++) Thread.Sleep(1000);
-          return 1;
+          return 0;
         default:
           while (true) { };
       }
     }
     static bool TaskEndedOrCanceled(Thread task)
     {
-      if (!task.IsAlive || (task.ThreadState & ThreadState.Aborted) == ThreadState.Aborted) return true;
-      return false; 
+      if (!task.IsAlive || (task.ThreadState & ThreadState.Aborted) == ThreadState.Aborted || (task.ThreadState & ThreadState.Stopped) == ThreadState.Stopped) return true;
+      return false;
     }
     static bool TaskCanceled(Thread task)
     {
